5c3ced46cfb4212c9a3115e4bfa639168f0fa02d
Taehyeon88 <Taehyeon5376@naver.com>
2024-08-02 10:05:19 +0900

    [feat] 플퍼그 수정

--- VRCon/Assets/Scripts/VR/UIManager.cs
+++ VRCon/Assets/Scripts/VR/UIManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject text1;
+    public string Massage;
+    public float Speed = 0.2f;
     void Start()
     {
         
@@ -13,6 +16,9 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (text1 == null)
+        {
+            text1.text 
+        }
     }
 }
