60e6e69b2d483f5d7b09b125e2eb9d7f8868d41b
Taehyeon88 <Taehyeon5376@naver.com>
2024-08-02 11:42:22 +0900

    [Feat]UI 스크립트

--- VRCon/Assets/Scripts/VR/UIManager.cs
+++ VRCon/Assets/Scripts/VR/UIManager.cs
@@ -5,20 +5,57 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject text1;
-    public string Massage;
-    public float Speed = 0.2f;
-    void Start()
+    public Text[] text;
+    public GameObject TextBox;
+    public string[] dialogue;
+
+    bool End = false;
+    int k;
+
+    //상황에 따른 변수 추가
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (text1 == null)
+        if (Input.GetKeyDown(KeyCode.Y))
         {
-            text1.text 
+            TextBox.SetActive(false);
+            text[k].text = null;
+        }
+
+        //실험
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            int j = 0;
+            Typing(j);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            int j = 1;
+            Typing(j);
+        }
+    }
+
+    public void Typing(int j)  //UI의 Text를 나오는 함수실
+    {
+        TextBox.SetActive(true);
+        StartCoroutine(Typing(dialogue[j], j));
+
+        //상황에 따른 j변수 바꾸기 추가
+    }
+
+    IEnumerator Typing(string talk, int j)
+    {
+
+        for (int i = 0; i < talk.Length; i++)
+        {
+            text[j].text += talk[i];
+            k = j;
+
+            yield return new WaitForSeconds(0.05f);
         }
     }
 }
